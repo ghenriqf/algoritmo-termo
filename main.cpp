192d252f18dfc822767a811df0a39063fd4597b5
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-06-02 21:03:24 -0300

    Implementa função de formatação de data com ctime para uso na seleção da palavra do dia

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <ctime>
+
 using namespace std;
+
+
+string dataAtual();
 
 int main(){
 
+    cout << dataAtual();
+
     return 0;
+
+
 }
+
+string dataAtual(){
+
+    time_t agora = time(0);
+
+    tm* tempo = localtime(&agora);
+
+    char dataFormatada[11];
+
+    strftime(dataFormatada, sizeof(dataFormatada), "%d/%m/%Y", tempo);
+
+    return dataFormatada;
+}
+
