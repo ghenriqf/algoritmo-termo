91462842bde397cfdf16058574e6bd2da0a8c98b
Murilo Pavanello <8222800@univali.br>
2025-06-03 20:55:23 -0300

    Substituídas bibliotecas para manipulação de CSV e implementado parser simples para verificação de dados

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <ctime>
-#include "csv.hpp"
 #include <string.h>
 #include <windows.h>
-
+#include <fstream>
+#include <sstream>
 
 using namespace std;
-
 
 string dataAtual();
 
@@ -15,8 +14,29 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
+    ifstream arquivo("palavras.csv");
 
+    string linha;
+
+    string data = dataAtual();
+
+    while(getline(arquivo, linha)){
+        stringstream ss(linha);
+
+        string valor;
+
+        while(getline(ss, valor, ',')){
+            if(valor == data){
+                while(getline(ss,valor)){
+                    cout << valor;
+                }
+
+            }
+        }
+    }
 }
+
+
 
 string dataAtual(){
 
